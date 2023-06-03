ccb7d536db4ecd22d76686ed030b847d8d6f780c
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2023-06-03 15:35:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// const divContainer = document.querySelector ('#linksandinfo');
+// let isClicked = true;
+
+
+// function showHide(){
+//     if(isClicked){
+//         divContainer.style.display = 'block';
+//         isClicked = false;
+//     }else{
+//         divContainer.style.display ='none';
+//         isClicked = true;
+// }
+// }
+
+// const divContainer = document.querySelector ('#linksandinfo');
+// let isClicked = true;
+
+
+// function showHide(){
+//     if(isClicked){
+//         divContainer.style.display = 'block';
+//         isClicked = false;
+//     }else{
+//         divContainer.style.display ='none';
+//         isClicked = true;
+// }
+// }
